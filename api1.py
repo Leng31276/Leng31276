b0f80309c857268900d8081476c45fb758c4a332
aitcoth <igregg.it@gmail.com>
2021-08-10 17:05:26 +0700

    add datetime

--- api1.py
+++ api1.py
@@ -1,12 +1,20 @@
-import vmanage
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 import csv
 import json
-import requests
+import vmanage
 
 auth = vmanage.Authentication()
 api = vmanage.API()
 
 path = "/statistics/interface/aggregation"
+
+tz = ZoneInfo("Asia/Bangkok")
+d_now = datetime.now(tz=tz)
+d_now_utc = d_now - d_now.utcoffset()
+d2 = d_now_utc.strftime("%Y-%m-%dT%H:00:00 UTC")
+d1 = (d_now_utc - timedelta(days=1)).strftime("%Y-%m-%dT%H:00:00 UTC")
 
 payload = json.dumps({
   "query": {
@@ -14,8 +22,8 @@ payload = json.dumps({
     "rules": [
       {
         "value": [
-          "2021-08-01T17:00:00 UTC",
-          "2021-08-02T16:59:59 UTC"
+          d1,
+          d2
         ],
         "field": "entry_time",
         "type": "date",
@@ -54,8 +62,16 @@ payload = json.dumps({
         "type": "max"
       },
       {
+        "property": "rx_octets",
+        "type": "sum"
+      },
+      {
         "property": "tx_kbps",
         "type": "max"
+      },
+      {
+        "property": "tx_octets",
+        "type": "sum"
       }
     ]
   }
@@ -63,10 +79,12 @@ payload = json.dumps({
 
 response = api.send_request("POST", path, auth.headers, payload)
 
+# debug output
 # print(json.dumps(response, indent=4))
 
-with open("api1.csv", 'w', newline='', encoding='utf-8') as output:  
+filename = "api1-{}.csv".format(d_now.strftime("%Y-%m-%d"))
+with open(filename, 'w', newline='', encoding='utf-8') as output:  
     writer = csv.writer(output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['system-ip', 'hostname', 'interface', 'rx_kbps', 'tx_kbps'])
+    writer.writerow(['system-ip', 'hostname', 'interface', 'max_rx_kbps', 'sum_rx_octets', 'max_tx_kbps', 'sum_tx_octets', 'sample', 'start_datetime', 'end_datetime'])
     for row in response['data']:
-        writer.writerow([row['vmanage_system_ip'], row['host_name'], row['interface'], row['rx_kbps'], row['tx_kbps']])
+        writer.writerow([row['vmanage_system_ip'], row['host_name'], row['interface'], row['rx_kbps'], row['rx_octets'], row['rx_kbps'], row['tx_octets'], row['count'], d1, d2])
