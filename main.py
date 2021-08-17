a2996b60f0e3306c6bf540f9c0fd227085ea14e0
aitcoth <igregg.it@gmail.com>
2021-08-17 16:08:38 +0700

    update files

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
+from datetime import date
+from pathlib import Path
 
+from api import settings
 from api.api1 import api1
+from api.packages import excel_pivot
 
 if __name__ == '__main__':
-    api1.run()
+    report_date = date(2021, 8, 10)
+    api1.run(report_date)
+
+    # api1_csv_dir = Path.joinpath(settings.BASE_DIR, "api1/csv")
+    # df = excel_pivot.import_csv(api1_csv_dir)
+    # table = excel_pivot.create_pivot(df, index_list=['end_datetime', 'hostname', 'interface'], value_list=['max_rx_kbps', 'sum_rx_octets', 'max_tx_kbps', 'sum_tx_octets'])
+    # api1_report_dir = Path.joinpath(settings.BASE_DIR, "api1")
+    # excel_pivot.save_report(table, api1_csv_dir/'report.xlsx')
+
+    print('Done!')
