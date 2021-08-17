fb240181f643cb5903952720dada1be92fdc9069
aitcoth <igregg.it@gmail.com>
2021-08-17 16:06:22 +0700

    add local env variable

--- api/settings.py
+++ api/settings.py
@@ -1,9 +1,11 @@
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
-VMANAGE_HOST = "10.0.253.21"
-VMANAGE_PORT = "8443"
-VMANAGE_USER = "admin"
-VMANAGE_PASS = "admin"
+VMANAGE_HOST = os.getenv('VMANAGE_HOST', "localhost")
+VMANAGE_PORT = os.getenv('VMANAGE_PORT', "443")
+VMANAGE_USER = os.getenv('VMANAGE_USER', "admin")
+VMANAGE_PASS = os.getenv('VMANAGE_PASS', "admin")
